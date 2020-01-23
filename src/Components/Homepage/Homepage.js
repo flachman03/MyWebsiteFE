68a685cf42568e4812388206d626e8fad49590cd
RyanFlachman <ryan@flachman.net>
2020-01-23 15:04:07 -0700

    Add background image and css styling

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Homepage.scss'
+
+const Homepage = () => {
+  return(
+    <main className='homepage__main'>
+      <div className='homepage__text-container'>
+        <h1 className='homepage__name'>Ryan Flachman</h1>
+        <h2 className='homepage__job'>Fullstack Developer</h2>
+      </div>
+    </main>
+  )
+}
+
+export default Homepage;
